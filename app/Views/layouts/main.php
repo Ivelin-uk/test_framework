0f4e62803765aa07a6d237a1381c65dd9d1e9338
Ivelin-uk <ivoagaininwow@gmail.com>
2025-09-29 11:05:07 +0300

    add

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -9,15 +9,12 @@
 <body class="bg-body-tertiary">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="<?= $this->url() ?>">🚀 Моят Framework</a>
+            <span class="navbar-brand">🚀 Моят Framework</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="<?= $this->url() ?>">Начало</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= $this->url('about') ?>">За нас</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= $this->url('contact') ?>">Контакти</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?= $this->url('users') ?>">Потребители</a></li>
                 </ul>
             </div>
